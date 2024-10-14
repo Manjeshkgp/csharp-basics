6c421c5c8561cb51559532daed6d520a9de16069
Manjesh Kumar Sharma <manjeshkgp@gmail.com>
2024-10-15 01:43:21 +0530

    user inputs

--- Program.cs
+++ Program.cs
@@ -4,39 +4,19 @@ namespace HelloWorld
 {
     class Program {
         static void Main(){
-            // WriteSomething();
-            // WriteSomethingSpecific("This is a sample string arg");
-            int a = 40;
-            int b = 20;
-            int result = Add(a,b);
-            Console.WriteLine("a = "+a+" b = "+b+" a+b = "+ result);
-            result = Divide(a,b);
-            Console.WriteLine("a = "+a+" b = "+b+" a/b = "+ result);
-            result = Multiply(a,b);
-            Console.WriteLine("a = "+a+" b = "+b+" a*b = "+ result);
-            result = Subtract(a,b);
-            Console.WriteLine("a = "+a+" b = "+b+" a-b = "+ result);
+            // string input = Console.ReadLine(); 
+            Console.WriteLine(Calculate());  
         }
+        public static int Calculate(){
+            Console.WriteLine("Please enter the first number");
+            string num1Input = Console.ReadLine();
+            Console.WriteLine("Please enter the second number");
+            string num2Input = Console.ReadLine();
 
-        public static int Add(int Param1, int Param2){
-            return Param1 + Param2;
-        }
-        public static int Subtract(int Param1, int Param2){
-            return Param1 - Param2;
-        }
-        public static int Divide(int Param1, int Param2){
-            return Param1 / Param2;
-        }
-        public static int Multiply(int Param1, int Param2){
-            return Param1 * Param2;
-        }
-        public static void WriteSomething(){
-            Console.WriteLine("I'm a called from a method");
-            Console.Read();
-        }
-        public static void WriteSomethingSpecific(string arg){
-            Console.WriteLine(arg);
-            Console.Read();
+            int num1 = int.Parse(num1Input);
+            int num2 = int.Parse(num2Input);
+            int result = num1 + num2;
+            return result;
         }
     }
 }
