28343471d8af659819dc80fdd0554fbeaefbb8cb
Manjesh Kumar Sharma <manjeshkgp@gmail.com>
2024-10-20 02:30:25 +0530

    try catch learnt

--- Program.cs
+++ Program.cs
@@ -1,22 +1,44 @@
 ﻿using System;
 
-namespace HelloWorld
+namespace TryAndCatch
 {
-    class Program {
-        static void Main(){
-            // string input = Console.ReadLine(); 
-            Console.WriteLine(Calculate());  
-        }
-        public static int Calculate(){
-            Console.WriteLine("Please enter the first number");
-            string num1Input = Console.ReadLine();
-            Console.WriteLine("Please enter the second number");
-            string num2Input = Console.ReadLine();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            string input = Console.ReadLine();
 
-            int num1 = int.Parse(num1Input);
-            int num2 = int.Parse(num2Input);
-            int result = num1 + num2;
-            return result;
+            try
+            {
+                int userInputNum = int.Parse(input);
+                try
+                {
+                    int result = userInputNum / 0;
+                }
+                catch (Exception)
+                {
+                    System.Console.WriteLine("Not dividable by 0");
+                }
+            }
+            catch (FormatException)
+            {
+                System.Console.WriteLine("Format Exception Error");
+            }
+            catch (OverflowException)
+            {
+                System.Console.WriteLine("Overflow exception, when you write very big numbers that int32 can't handle");
+            }
+            catch (Exception)
+            {
+                System.Console.WriteLine("Unknown/all other errors cacthed");
+            }
+            finally
+            {
+                System.Console.WriteLine("This will be called anyways");
+            }
+
+            Console.ReadKey();
         }
     }
 }
