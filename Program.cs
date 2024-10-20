1d57e224ccc6a35cef470564bd4a564e66e354c5
Manjesh Kumar Sharma <manjeshkgp@gmail.com>
2024-10-20 18:38:40 +0530

    operators learnt

--- Program.cs
+++ Program.cs
@@ -1,44 +1,76 @@
 ﻿using System;
 
-namespace TryAndCatch
+namespace Operators
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter a number");
-            string input = Console.ReadLine();
+           int num1 = 5;
+           int num2 = 3;
+           int num3;
 
-            try
-            {
-                int userInputNum = int.Parse(input);
-                try
-                {
-                    int result = userInputNum / 0;
-                }
-                catch (Exception)
-                {
-                    System.Console.WriteLine("Not dividable by 0");
-                }
-            }
-            catch (FormatException)
-            {
-                System.Console.WriteLine("Format Exception Error");
-            }
-            catch (OverflowException)
-            {
-                System.Console.WriteLine("Overflow exception, when you write very big numbers that int32 can't handle");
-            }
-            catch (Exception)
-            {
-                System.Console.WriteLine("Unknown/all other errors cacthed");
-            }
-            finally
-            {
-                System.Console.WriteLine("This will be called anyways");
-            }
+           // unary operators
+           num3 = -num1;
+           System.Console.WriteLine("num3 is {0}",num3);
 
-            Console.ReadKey();
+            bool isSunny = true;
+           System.Console.WriteLine("isSunny is {0}",!isSunny);
+
+            // increment operators
+
+            num2++;
+            System.Console.WriteLine("23num2 is {0}",num2);
+            System.Console.WriteLine("24num2 is {0}",num2++); // it will change in 25num2 not here, here it is 4
+            System.Console.WriteLine("25num2 is {0}",num2); // here num2 is 5
+            // pre increment
+            System.Console.WriteLine("26num2 is {0}",++num2); // but here num2 is increased directly and logged as 6
+
+            // decrement operators
+
+            num2--;
+            System.Console.WriteLine("32num2 is {0}",num2);
+            System.Console.WriteLine("33num2 is {0}",num2--);
+            System.Console.WriteLine("34num2 is {0}",num2);
+            // pre decrement
+            System.Console.WriteLine("35num2 is {0}",--num2);
+
+            // modular operators
+
+            int result;
+            result = num1 % num2; // after dividing, gives remainder
+            System.Console.WriteLine("{0} % {1} = {2}",num1,num2,result);
+
+            // relational & type operators
+
+            bool isLower;
+
+            isLower = num1 < num2;
+            System.Console.WriteLine("result of {0} < {1} = {2}",num1,num2,isLower);
+
+            // equality operators
+
+            bool isEqual;
+
+            isEqual = num2 == 3;
+            System.Console.WriteLine("result of num2 == 3 = {0}",isEqual);
+
+            isEqual = num2 != 3;
+            System.Console.WriteLine("result of num2 != 3 = {0}",isEqual);
+
+            // conditional operators
+
+            bool isLowerAndSunny;
+            // AND operator
+            isLowerAndSunny = isLower && isSunny;
+            System.Console.WriteLine("result of isLower && isSunny is {0}",isLowerAndSunny);
+
+            bool isLowerOrSunny;
+            // OR operator
+            isLowerOrSunny = isLower || isSunny;
+            System.Console.WriteLine("result of isLower || isSunny is {0}",isLowerOrSunny);
+            
+           Console.ReadKey(); // it helps to exit the program only after entering any key
         }
     }
 }
