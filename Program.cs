da60895d6abc2e3c214f2eb34dcd49f502802887
Manjesh Kumar Sharma <manjeshkgp@gmail.com>
2024-10-20 19:04:29 +0530

    learnt tryparse

--- Program.cs
+++ Program.cs
@@ -6,16 +6,29 @@ namespace IfStatements
     {
         static void Main(string[] args)
         {
-            int temparature = 10;
+            int num1 = 10;
+            System.Console.WriteLine("Please enter a number to compare");
+            string num2Str = Console.ReadLine();
+            if (int.TryParse(num2Str, out int num2))
+            {
+                System.Console.WriteLine("The statement that {0} is greater than {1} is {2}", num1, num2, num1 > num2);
+            }
+            else
+            {
+                System.Console.WriteLine("Parsing failed");
+            }
+            System.Console.WriteLine("Now let's learn the parsing in simple way, enter anything as of now");
 
-            if(temparature<10){
-                System.Console.WriteLine("temparature is cold");
+            string input = Console.ReadLine();
+            int inputNum;
+            bool isParsable = int.TryParse(input, out inputNum);
+            if (isParsable)
+            {
+                System.Console.WriteLine("You entered an integer that is {0}", inputNum);
             }
-            else if(temparature==10){
-                System.Console.WriteLine("temparature is 10 degree Celcius");
-            }
-            else{
-                System.Console.WriteLine("temparature is greater than 10 degree Celcius");
+            else
+            {
+                System.Console.WriteLine("Your input was not an int32 integer");
             }
 
             Console.ReadKey(); // it helps to exit the program only after entering any key
