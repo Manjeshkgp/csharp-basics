1d9f16ee4cc17cfe6fd33e4ab2b9891b4107c0da
Manjesh Kumar Sharma <manjeshkgp@gmail.com>
2024-10-20 19:33:50 +0530

    switch case learnt

--- Program.cs
+++ Program.cs
@@ -1,34 +1,24 @@
 ﻿using System;
 
-namespace IfStatements
+namespace SwitchCase
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int num1 = 10;
-            System.Console.WriteLine("Please enter a number to compare");
-            string num2Str = Console.ReadLine();
-            if (int.TryParse(num2Str, out int num2))
-            {
-                System.Console.WriteLine("The statement that {0} is greater than {1} is {2}", num1, num2, num1 > num2);
-            }
-            else
-            {
-                System.Console.WriteLine("Parsing failed");
-            }
-            System.Console.WriteLine("Now let's learn the parsing in simple way, enter anything as of now");
+            int age = 20;
 
-            string input = Console.ReadLine();
-            int inputNum;
-            bool isParsable = int.TryParse(input, out inputNum);
-            if (isParsable)
+            switch (age)
             {
-                System.Console.WriteLine("You entered an integer that is {0}", inputNum);
-            }
-            else
-            {
-                System.Console.WriteLine("Your input was not an int32 integer");
+                case 15:
+                System.Console.WriteLine("You are a teenager");
+                break;
+                case 20:
+                System.Console.WriteLine("You must have a moustache, you are 20");
+                break; // you can use return; instead of break; as well, no error will be thrown
+                default:
+                System.Console.WriteLine("I'm not sure, how old you are");
+                break;
             }
 
             Console.ReadKey(); // it helps to exit the program only after entering any key
