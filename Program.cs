98e64436a6ac3c828dfa22f70949a37b243510bc
Manjesh Kumar Sharma <manjeshkgp@gmail.com>
2024-10-20 18:49:03 +0530

    if statements

--- Program.cs
+++ Program.cs
@@ -1,76 +1,24 @@
 ﻿using System;
 
-namespace Operators
+namespace IfStatements
 {
     class Program
     {
         static void Main(string[] args)
         {
-           int num1 = 5;
-           int num2 = 3;
-           int num3;
+            int temparature = 10;
 
-           // unary operators
-           num3 = -num1;
-           System.Console.WriteLine("num3 is {0}",num3);
+            if(temparature<10){
+                System.Console.WriteLine("temparature is cold");
+            }
+            else if(temparature==10){
+                System.Console.WriteLine("temparature is 10 degree Celcius");
+            }
+            else{
+                System.Console.WriteLine("temparature is greater than 10 degree Celcius");
+            }
 
-            bool isSunny = true;
-           System.Console.WriteLine("isSunny is {0}",!isSunny);
-
-            // increment operators
-
-            num2++;
-            System.Console.WriteLine("23num2 is {0}",num2);
-            System.Console.WriteLine("24num2 is {0}",num2++); // it will change in 25num2 not here, here it is 4
-            System.Console.WriteLine("25num2 is {0}",num2); // here num2 is 5
-            // pre increment
-            System.Console.WriteLine("26num2 is {0}",++num2); // but here num2 is increased directly and logged as 6
-
-            // decrement operators
-
-            num2--;
-            System.Console.WriteLine("32num2 is {0}",num2);
-            System.Console.WriteLine("33num2 is {0}",num2--);
-            System.Console.WriteLine("34num2 is {0}",num2);
-            // pre decrement
-            System.Console.WriteLine("35num2 is {0}",--num2);
-
-            // modular operators
-
-            int result;
-            result = num1 % num2; // after dividing, gives remainder
-            System.Console.WriteLine("{0} % {1} = {2}",num1,num2,result);
-
-            // relational & type operators
-
-            bool isLower;
-
-            isLower = num1 < num2;
-            System.Console.WriteLine("result of {0} < {1} = {2}",num1,num2,isLower);
-
-            // equality operators
-
-            bool isEqual;
-
-            isEqual = num2 == 3;
-            System.Console.WriteLine("result of num2 == 3 = {0}",isEqual);
-
-            isEqual = num2 != 3;
-            System.Console.WriteLine("result of num2 != 3 = {0}",isEqual);
-
-            // conditional operators
-
-            bool isLowerAndSunny;
-            // AND operator
-            isLowerAndSunny = isLower && isSunny;
-            System.Console.WriteLine("result of isLower && isSunny is {0}",isLowerAndSunny);
-
-            bool isLowerOrSunny;
-            // OR operator
-            isLowerOrSunny = isLower || isSunny;
-            System.Console.WriteLine("result of isLower || isSunny is {0}",isLowerOrSunny);
-            
-           Console.ReadKey(); // it helps to exit the program only after entering any key
+            Console.ReadKey(); // it helps to exit the program only after entering any key
         }
     }
 }
