b92022bcc74cdf8ef0217177bc3843f758e3a2f6
Manjesh Kumar Sharma <manjeshkgp@gmail.com>
2024-10-26 19:39:10 +0530

    arrays basic

--- Program.cs
+++ Program.cs
@@ -1,23 +1,27 @@
 ﻿using System;
-using MembersC;
-using Properties;
 
-namespace Class_Basics
+namespace Arrays
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Box box = new Box();
-            box.Height = 10;
-            box.Width = 20;
-            box.SetLength(5);
-            System.Console.WriteLine("Length of the box is {0}",box.GetLength());
-            System.Console.WriteLine("Width of the box is {0}",box.Width);
-            box.DisplayInfo();
+            int [] grades = new int[5]; // an array with 5 elements
+            grades[0] = 80;
+            grades[1] = 15;
+            grades[2] = 28;
+            grades[3] = 38;
+            grades[4] = -70;
 
-            Members members1 = new Members();
-            members1.Introducing(false);
+            // if you try to log the grades directly you will only be able to log dataType[], so you need to log elements one by one
+            // another way of initializing array
+            int [] sampleArr = {10,20,30,40,50};
+
+            // 3rd way of initializing array
+            int [] sampleArr2 = new int[] {10,20,30,40,50};
+
+            System.Console.WriteLine("Length of sampleArr is {0}",sampleArr.Length);
+
             Console.ReadKey();
         }
 
